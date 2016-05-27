60da201e051c89d077fa3c21d8bea666e67a26e5
noborio yoshiki <test>
2016-05-27 15:58:30 +0900

    a

--- ChocoBall/LockOn.h
+++ ChocoBall/LockOn.h
@@ -9,7 +9,10 @@ public:
 	~CLockOn();
 	float LockOnRotation(float _X, D3DXVECTOR3 position, int lockonEnemyIndex);
 	int FindNearEnemy(D3DXVECTOR3 position);
+	float LockOnRotation2(float _X, D3DXVECTOR3 position, int lockonBlockIndex);
+	int FindNearBlock(D3DXVECTOR3 position);
 private:
-	bool            LockOnflag;			//ロックオンフラグ
-	int				m_lockonEnemyIndex;	//ロックオンしている敵のインデックス。
+	bool LockOnflag;		//ロックオンフラグ
+	int	m_lockonEnemyIndex;	//ロックオンしている敵のインデックス。
+	int m_lockonBlock;		//ロックオンしているブロックのインデックス
 };
