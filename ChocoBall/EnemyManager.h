abbbca9940c3524608149884134dd70801877db6
fujita <t>
2016-06-07 17:57:20 +0900

    k

--- ChocoBall/EnemyManager.h
+++ ChocoBall/EnemyManager.h
@@ -32,9 +32,6 @@ public:
 		if (enemyIndex == -1){
 			return;
 		}
-		//else{
-		//	//SAFE_DELETE(Enemy[enemyIndex]);
-		//}
 		for (int i = enemyIndex; i < numEnemy - 1; i++){
 			Enemy[i] = Enemy[i + 1];
 		}
