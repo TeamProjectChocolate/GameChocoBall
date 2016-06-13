6167b0e11ddc6f52b2904636f64304e933480903
noborio yoshiki <test>
2016-06-13 18:21:29 +0900

    入口のステージ１の追加

---
+++ ChocoBall/PlayerPositionInfoStage3.h
@@ -0,0 +1,6 @@
+﻿//Player
+{
+	D3DXVECTOR3(0.00f, -3.00f, 18.00f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.00f, 1.00f, 1.00f ),            //拡大
+},
---
+++ ChocoBall/collisionInfoStage3.h
@@ -0,0 +1,48 @@
+﻿//Ground_00
+{
+	D3DXVECTOR3(0.00f, -6.23f, 14.64f),             //平行移動
+	D3DXQUATERNION(0.25f, 0.00f, 0.00f, 0.97f ),    //回転
+	D3DXVECTOR3(8.00f, 2.00f, 23.00f ),            //拡大
+},
+//Ground_01
+{
+	D3DXVECTOR3(-12.80f, -0.40f, -9.64f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(34.00f, 1.00f, 29.00f ),            //拡大
+},
+//Ground_02
+{
+	D3DXVECTOR3(-25.80f, 4.77f, 10.67f),             //平行移動
+	D3DXQUATERNION(-0.25f, 0.00f, 0.00f, 0.97f ),    //回転
+	D3DXVECTOR3(8.00f, 2.00f, 23.00f ),            //拡大
+},
+//Ground_03
+{
+	D3DXVECTOR3(-15.00f, 10.20f, 34.90f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(40.00f, 2.00f, 29.00f ),            //拡大
+},
+//Ground_04
+{
+	D3DXVECTOR3(0.00f, 15.73f, 14.67f),             //平行移動
+	D3DXQUATERNION(0.25f, 0.00f, 0.00f, 0.97f ),    //回転
+	D3DXVECTOR3(8.00f, 2.00f, 23.00f ),            //拡大
+},
+//Ground_05
+{
+	D3DXVECTOR3(-15.00f, 21.10f, -9.47f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(40.00f, 2.00f, 29.00f ),            //拡大
+},
+//Ground_06
+{
+	D3DXVECTOR3(-25.80f, 21.17f, 0.64f),             //平行移動
+	D3DXQUATERNION(-0.25f, 0.00f, 0.00f, 0.97f ),    //回転
+	D3DXVECTOR3(8.00f, 2.00f, 23.00f ),            //拡大
+},
+//Ground_07
+{
+	D3DXVECTOR3(-26.50f, 26.51f, 15.39f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(10.00f, 2.00f, 9.90f ),            //拡大
+},
