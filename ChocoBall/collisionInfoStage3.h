5cf6c86a4379e906329eb848334dc18eca6e1286
doi <a>
2016-06-14 12:08:57 +0900

    ゆにちぃ

--- ChocoBall/collisionInfoStage3.h
+++ ChocoBall/collisionInfoStage3.h
@@ -46,3 +46,75 @@
 	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(10.00f, 2.00f, 9.90f ),            //拡大
 },
+//Wall_00
+{
+	D3DXVECTOR3(3.55f, 9.20f, 14.10f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.00f, 42.00f, 74.00f ),            //拡大
+},
+//Wall_01
+{
+	D3DXVECTOR3(-29.30f, 15.80f, 11.10f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.00f, 38.00f, 82.00f ),            //拡大
+},
+//Wall_02
+{
+	D3DXVECTOR3(-22.27f, 15.30f, 12.57f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.00f, 36.00f, 45.00f ),            //拡大
+},
+//Wall_03
+{
+	D3DXVECTOR3(-3.39f, 15.30f, 12.57f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.00f, 56.00f, 45.00f ),            //拡大
+},
+//Wall_04
+{
+	D3DXVECTOR3(-4.00f, 15.14f, -11.03f),             //平行移動
+	D3DXQUATERNION(0.00f, -0.53f, 0.00f, 0.85f ),    //回転
+	D3DXVECTOR3(3.00f, 31.00f, 1.00f ),            //拡大
+},
+//Wall_05
+{
+	D3DXVECTOR3(-6.66f, 13.10f, -13.71f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.37f, 0.00f, 0.93f ),    //回転
+	D3DXVECTOR3(2.00f, 33.00f, 5.00f ),            //拡大
+},
+//Wall_06
+{
+	D3DXVECTOR3(-9.42f, 12.67f, -15.82f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.56f, 0.00f, 0.83f ),    //回転
+	D3DXVECTOR3(2.00f, 34.00f, 3.00f ),            //拡大
+},
+//Wall_07
+{
+	D3DXVECTOR3(-12.95f, 14.80f, -16.30f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(5.00f, 30.00f, 2.00f ),            //拡大
+},
+//Wall_08
+{
+	D3DXVECTOR3(-16.64f, 15.40f, -16.25f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.20f, 0.00f, 0.98f ),    //回転
+	D3DXVECTOR3(3.00f, 35.00f, 1.00f ),            //拡大
+},
+//Wall_09
+{
+	D3DXVECTOR3(-19.60f, 14.20f, -13.99f),             //平行移動
+	D3DXQUATERNION(0.00f, 0.39f, 0.00f, 0.92f ),    //回転
+	D3DXVECTOR3(5.00f, 34.00f, 1.00f ),            //拡大
+},
+//Wall_10
+{
+	D3DXVECTOR3(-21.62f, 0.82f, -11.43f),             //平行移動
+	D3DXQUATERNION(0.00f, -0.19f, 0.00f, 0.98f ),    //回転
+	D3DXVECTOR3(1.00f, 1.00f, 2.00f ),            //拡大
+},
+//Wall_11
+{
+	D3DXVECTOR3(-21.93f, 0.81f, -10.67f),             //平行移動
+	D3DXQUATERNION(0.00f, -0.19f, 0.00f, 0.98f ),    //回転
+	D3DXVECTOR3(1.00f, 1.00f, 2.00f ),            //拡大
+},
