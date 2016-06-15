ec00be43cf18c335ac61a814602b5181d268b88f
noborio yoshiki <test>
2016-06-15 18:53:27 +0900

    a

--- ChocoBall/GimmickTriggerInfoStage3.h
+++ ChocoBall/GimmickTriggerInfoStage3.h
@@ -1,6 +1,6 @@
 ﻿//GimmickTrigger
 {
-	D3DXVECTOR3(9.92f, 4.00f, 0.00f),             //平行移動
+	D3DXVECTOR3(0.00f, 4.00f, 0.00f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(7.00f, 8.00f, 1.00f ),            //拡大
 },
