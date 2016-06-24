596ec319aaa0d33cb05eefc3398510c59814e854
fujita <t>
2016-06-24 18:58:14 +0900

    pushするよー

--- ChocoBall/GimmickTriggerInfoStage3.h
+++ ChocoBall/GimmickTriggerInfoStage3.h
@@ -10,3 +10,15 @@
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.00f, 4.69f, 8.03f ),            //拡大
 },
+//GimmickTrigger
+{
+	D3DXVECTOR3(-60.39f, 18.89f, -27.51f),             //平行移動
+	D3DXQUATERNION(0.00, 1.00f, 0.00f, 0.00f ),    //回転
+	D3DXVECTOR3(1.00f, 4.69f, 8.03f ),            //拡大
+},
+//GimmickTrigger
+{
+	D3DXVECTOR3(-75.08f, 11.96f, -17.26f),             //平行移動
+	D3DXQUATERNION(0.00, -0.92f, 0.00f, 0.38f ),    //回転
+	D3DXVECTOR3(1.00f, 4.69f, 8.03f ),            //拡大
+},
--- ChocoBall/GimmickTriggerInfoStage5.h
+++ ChocoBall/GimmickTriggerInfoStage5.h
@@ -10,3 +10,9 @@
 	D3DXQUATERNION(0.00, -0.71f, 0.00f, 0.71f ),    //回転
 	D3DXVECTOR3(39.22f, 12.88f, 1.00f ),            //拡大
 },
+//GimmickTrigger
+{
+	D3DXVECTOR3(-11.40f, 33.70f, -67.40f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(14.41f, 5.45f, 1.00f ),            //拡大
+},
